0d17bf60f190bd5cc8c56e116148a8025d4bd07c
Lijun Yu <lijun@lj-y.com>
2022-02-28 04:34:58 +0000

    more options for run_cnn

--- spring2022/hw2/code/run_cnn.py
+++ spring2022/hw2/code/run_cnn.py
@@ -10,18 +10,31 @@ from stages import CNNFeature, LoadVideo, SaveFeature
 class ExtractCNNFeature(System):
 
     def get_num_pipeline(self, resources, *, args):
+        """Returns desired number of pipelines to run in parallel."""
         self.args = args
-        return max(1, len(resources.get('gpu')))
+        self.resources = resources.select(cpu=True)
+        # Manual allocation of GPU resources for multi-pipeline per GPU setup
+        gpu_resources = resources.select(gpu=True).split(
+            len(resources.get('gpu')))
+        self.gpu_resources = gpu_resources * self.args.pipeline_per_gpu
+        if len(self.gpu_resources) == 0:
+            # CPU only
+            return max(1, len(resources.get('cpu')) // 2)
+        else:
+            return len(self.gpu_resources)
 
     def get_stages(self, resources):
-        resources_cpu = resources.select(cpu=True)
+        """Returns stages for the current pipeline. 
+           (Called once for each pipeline)"""
+        io_resources = resources.select(cpu=(0, 1))
+        cnn_resources = resources + self.gpu_resources.pop(0)
         stages = [
-            LoadVideo(resources_cpu.select(cpu=(0, 1)),
-                      video_dir=self.args.video_dir),
-            CNNFeature(resources, model_name='resnet18',
-                       node_name='avgpool', replica_per_gpu=4),
-            SaveFeature(resources_cpu.select(cpu=(0, 1)),
-                        feature_dir=self.args.cnn_dir),
+            LoadVideo(io_resources, video_dir=self.args.video_dir),
+            CNNFeature(cnn_resources, model_name='resnet18',
+                       node_name='avgpool',
+                       # Number of parallel workers in the stage
+                       replica_per_gpu=self.args.replica_per_gpu),
+            SaveFeature(io_resources, feature_dir=self.args.cnn_dir),
         ]
         return stages
 
@@ -34,6 +47,9 @@ def parse_args(argv=None):
             osp.dirname(__file__), '../data/videos'))
     parser.add_argument(
         '--cnn_dir', default=osp.join(osp.dirname(__file__), '../data/cnn'))
+    parser.add_argument('--pipeline_per_gpu', type=int, default=1)
+    parser.add_argument('--replica_per_gpu', type=int, default=4)
+    parser.add_argument('--job_timeout', type=int, default=10)
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args(argv)
     return args
@@ -55,7 +71,7 @@ def main(args):
     jobs = build_jobs(args)
     system.add_jobs(jobs)
     try:
-        for job in system.wait_jobs(len(jobs)):
+        for job in system.wait_jobs(len(jobs), job_timeout=args.job_timeout):
             continue
         system.end()
     except:
